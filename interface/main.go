9fed03b71adf22ef472f56f3f05a720dc82256f5
HG <hglkl6854@gmail.com>
2024-05-05 03:07:03 +0800

    feat: interface

---
+++ interface/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+// go 语言实现接口没有 implement 关键字，只需要定义与接口名称一致，且定义与所实现接口的全部抽象方法即可。鸭子类型
+type game interface {
+	start()
+	end()
+}
+
+type lol struct {
+
+}
+
+func (lol lol) start() {
+	fmt.Println("开始游戏")
+}
+
+func (lol lol) end() {
+	fmt.Println("结束游戏")
+}
+
+type computer struct{
+
+}
+
+func (computer *computer) worker(game game) {
+	game.start()
+	game.end()
+}
+
+func worker(game game)  {
+	game.start()
+	game.end()
+}
+
+func main()  {
+	lol := lol{}
+	computer := computer{}
+	computer.worker(lol)
+	worker(lol)
+}
