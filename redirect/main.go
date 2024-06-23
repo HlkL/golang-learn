87780dc1ac49ae25857f0ba5bcf937bfea4c48ca
HG <hglkl6854@gmail.com>
2024-06-23 18:07:51 +0800

    feat: 路由匹配

--- redirect/main.go
+++ redirect/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -27,6 +28,50 @@ func main() {
 			"msg": "ok",
 		})
 	})
+
+	// 匹配任意请求方式
+	r.Any("/any", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"msg": "any request method",
+		})
+	})
+
+	// 定义没有处理函数的路由
+	r.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(404, gin.H{
+			"msg": "page not found",
+		})
+	})
+
+	r.GET("/users", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "This is /users with GET")
+	})
+
+	// 定义 r.NoMethod() 在所有路由之后
+	r.HandleMethodNotAllowed = true
+	r.NoMethod(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusMethodNotAllowed, gin.H{
+			"msg": "method not allowed",
+		})
+	})
+
+	g := r.Group("/g")
+	{
+		g.GET("/get", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"msg": ctx.Request.URL.Path,
+			})
+		})
+
+		// 嵌套路由组
+		xx := g.Group("/xx")
+		xx.GET("oo", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"msg": ctx.Request.URL.Path,
+			})
+		})
+
+	}
 
 	r.Run()
 }
